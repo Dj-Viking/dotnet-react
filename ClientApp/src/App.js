0c598a0dbd6f5a7f9b6a3703688ddd3a8c93ddf5
dj-viking <anders.swedishviking@gmail.com>
2022-05-23 07:45:14 -0400

    moved router to app.js, converted app.js to fc

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -1,22 +1,22 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Route } from 'react-router';
 import { Layout } from './components/Layout';
+import { BrowserRouter } from 'react-router-dom';
 import { Home } from './components/Home';
 import { FetchData } from './components/FetchData';
 import { Counter } from './components/Counter';
 
 import './custom.css'
 
-export default class App extends Component {
-  static displayName = App.name;
-
-  render () {
-    return (
+export const App = () => {
+  const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
+  return (
+    <BrowserRouter basename={baseUrl}>
       <Layout>
         <Route exact path='/' component={Home} />
         <Route path='/counter' component={Counter} />
         <Route path='/fetch-data' component={FetchData} />
       </Layout>
-    );
-  }
+    </BrowserRouter>
+  );
 }
